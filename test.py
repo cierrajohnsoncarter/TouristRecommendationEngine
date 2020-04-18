2cbe7316eca35ac706611bfefaef2ddd48c8c033
cierrajohnsoncarter <cierrajohnsoncarter@outlook.com>
2020-04-18 19:19:39 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+#*Tests for functions in project uncomment print statements to test functions
+
+#*Tests 'get_destination_index' function. You can change the argument to any string in the 'destinations list'
+#print(get_destination_index('Paris, France'))
+
+#*Tests 'get_traveler_location' function. Below is a test traveler format you can use to create your own traveler to get the index of the travelers destination
+#test_traveler_2 = ['John Doe', 'Cairo, Egypt', [historical site, art]]
+#print(test_destination_index)
+
+#*Tests added 'attractions' to attractions list
+#print(attractions)
+
+#*Tests 'find_attractions' function by finding attractions in LA with arts as an interest
+#print(la_arts)
+
+#*Tests get_attractions_for_traveler_function by presenting a message with things the user might like based on thier interests
+#print(results_france)
